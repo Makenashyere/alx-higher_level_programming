fe9c89f8bea82e4e9578641a095b8ebae915e64e
Makena Hildah <makenabenedict@gmail.com>
2022-11-28 09:16:13 -0800

    python 0x00

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def magic_calcuation(a, b):
+    return (98 + a ** b)
---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/bin/usr/python3
+str = "Python is an interpreted, intractive, object-oriented programing\
+        language that cobines remarkable power with very clear syntax"
+        str = str[39:66] + str[106:1112] + str[:6]
+        print(str)
