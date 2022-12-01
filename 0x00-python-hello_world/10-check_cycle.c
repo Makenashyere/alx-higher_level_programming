15f144c8ac7097a48f2a90d2a6bfc69a1fe5b951
Makena Hildah <makenabenedict@gmail.com>
2022-12-01 03:52:39 -0800

    lists.h

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,19 +8,22 @@
  */
 int  check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+	listint_t *slow, *fast;
 
 	if (!list)
+	{
 		return (0);
-
+	}
+	slow = list;
+	fast = list->next;
 	while (slow && fast && fast->next)
 	{
+		if (slow == fast)
+		{
+			return (1);
+		}
 		slow = slow->next;
 		fast = fast->next->next;
-		if (slow == fast)
-			return (1);
 	}
-
 	return (0);
 }
