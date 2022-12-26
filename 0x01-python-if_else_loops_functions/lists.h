2f8d0338973eadf97b7828ae4651f90c524ee00d
Makena Hildah <makenabenedict@gmail.com>
2022-12-26 07:34:30 -0800

    0x01-python3

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -14,7 +14,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-size_t print_listint(constlistint_t *h);
+size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
