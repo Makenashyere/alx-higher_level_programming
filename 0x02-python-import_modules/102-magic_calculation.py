aee75409a6db363a7040c7a6339c7a8321c810c5
Makena Hildah <makenabenedict@gmail.com>
2022-12-26 09:44:22 -0800

    0x02-py

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    """Match bytecode provided by Holberton School."""
+    from magic_calcultion_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for i in rage(4, 6):
+            c = add 9c, i)
+        return (c)
+
+    else:
+        return(sub(a, b))
+
+
---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("{} argumrnts:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
