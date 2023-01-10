e7bb7a99872766ca4a810561b649d53e3a42badf
makena Hildah <makenabenedict@gmail.com>
2023-01-10 06:15:26 -0800

    0x0B-python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            print(line, end="")
+            
+read_file("my_tet_file.txt")
